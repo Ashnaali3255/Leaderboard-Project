640ef4e754c1400fe2e37f1f3bb82fae8f9aee0a
Ashnaali3255 <ashnaalipk@gmail.com>
2023-03-09 17:08:03 +0500

    Update dist, src folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 // import _ from 'lodash';
 import './style.css';
 import './modules/leader.js';
-// import getId from './modules/leader.js';
-// window.onload = async() => {
-
-//     console.log(await getId);
-// }
-

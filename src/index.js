70538b0dbd02b61d94c5f076e80dddc0adea9308
Ashnaali3255 <ashnaalipk@gmail.com>
2023-03-09 19:24:37 +0500

    Update html, js and styles.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-// import _ from 'lodash';
 import './style.css';
-import './modules/leader.js';
+import { getId, add, getScores } from './modules/leader.js';
+
+const refreshBtn = document.getElementById('refresh-btn');
+const submitBtn = document.getElementById('submit-btn');
+const table = document.getElementById('table');
+
+let id;
+
+window.onload = async () => {
+  id = await getId();
+  console.log('id', id);
+};
+const refresh = async () => {
+  table.innerHTML = '';
+  const gamerScores = await getScores(id);
+  const scores = await gamerScores.result;
+  console.log('gamers', scores);
+  scores.forEach((entry) => {
+    const listItems = document.createElement('tr');
+    listItems.className = 'listItems';
+    listItems.innerHTML = `
+    <td>${entry.user}</td>
+    <td>${entry.score}</td>
+`;
+    table.appendChild(listItems);
+  });
+};
+
+refreshBtn.addEventListener('click', async () => {
+  await refresh();
+});
+
+submitBtn.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const newScore = {
+    user: document.getElementById('name').value,
+    score: document.getElementById('score').value,
+  };
+  document.getElementById('name').value = '';
+  document.getElementById('score').value = '';
+  await add(newScore, id);
+});
--- src/modules/leader.js
+++ src/modules/leader.js
@@ -1,33 +1,31 @@
-const form = document.querySelector('.form-cont');
-const refreshBtn = document.getElementById('refresh-btn');
-const table = document.getElementById('table');
-
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
-const getScores = async () => {
-  const response = await fetch(`${url}`);
+
+// Get the game Id
+export const getId = async () => {
+  const fetchId = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      name: 'new game for me',
+    }),
+  });
+  const id = await fetchId.json();
+  return id.result.split(' ')[3];
+};
+
+// get the scores
+export const getScores = async (id) => {
+  console.log('my id', id);
+  const response = await fetch(`${url}${id}/scores/`);
   const data = await response.json();
   return data;
 };
 
-const refresh = () => {
-  table.innerHTML = '';
-  const gamers = [];
-  getScores().then((entry) => {
-    Object.entries(entry.result).forEach(([, value]) => {
-      gamers.push(JSON.stringify(value));
-      const listItems = document.createElement('tr');
-      listItems.className = 'listItems';
-      listItems.innerHTML = `
-                    <td>${value.user}</td>
-                    <td>${value.score}</td>
-           `;
-      table.appendChild(listItems);
-    });
-  });
-};
-
-const add = async (newScore) => {
-  const response = await fetch(`${url}`, {
+// add score
+export const add = async (newScore, id) => {
+  const response = await fetch(`${url}${id}/scores/`, {
     method: 'POST',
     headers: {
       'content-Type': 'application/json',
@@ -35,27 +33,7 @@ const add = async (newScore) => {
     body: JSON.stringify(newScore),
   });
   const data = await response.json();
-  refresh();
   return data;
 };
 
-const create = () => {
-  const newScore = {
-    user: document.getElementById('name').value,
-    score: document.getElementById('score').value,
-  };
-  document.getElementById('name').value = '';
-  document.getElementById('score').value = '';
-  add(newScore);
-};
-
-refresh();
-
-refreshBtn.addEventListener('click', () => {
-  refresh();
-});
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  create();
-});
+export default { getId, add, getScores };
